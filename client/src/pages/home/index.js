4d89e3e945202ff6cec3ff52e830baff1594cc9c
Saicharanpalagiri <charan517214@gmail.com>
2023-01-06 22:32:33 +0530

    third commit

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -5,7 +5,7 @@ import UserSearch from "./components/UserSearch";
 import UsersList from "./components/UsersList";
 import { io } from "socket.io-client";
 
-const socket = io('https://sheychat-udemy.onrender.com');
+const socket = io('https://whatsapp-clone-2.0.onrender.com');
 function Home() {
   const [searchKey, setSearchKey] = React.useState("");
   const { selectedChat, user } = useSelector((state) => state.userReducer);
