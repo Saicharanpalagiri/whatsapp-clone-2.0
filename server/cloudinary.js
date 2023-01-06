3e424fd06186e3fa036e2423205b69ae24fa5832
Saicharanpalagiri <charan517214@gmail.com>
2023-01-06 22:24:12 +0530

    second commit

---
+++ server/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "ddjhj2ra8",
+  api_key: "441177123442138",
+  api_secret: "n1wBsb3VkuWMx5y3Lua_RoLDiKE",
+});
+
+module.exports = cloudinary;
